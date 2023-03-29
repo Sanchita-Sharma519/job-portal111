7faf093b2059c02e1248ad9b284368e776c0705e
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-03-30 00:42:30 +0530

    Created User Model

--- backend/app.js
+++ backend/app.js
@@ -19,15 +19,23 @@ const authRoutes = require('./routes/authroutes');
 mongoose.connect("mongodb://127.0.0.1:27017/jobDB",{useNewUrlParser: true,useUnifiedTopology: true,
 });
 
+mongoose.connection.on('connecting', () => { 
+  console.log('DB connecting')
+ // console.log(mongoose.connection.readyState); //logs 2
+});
 mongoose.connection.on('connected', () => {
-  console.log('connected');
-  console.log(mongoose.connection.readyState); //logs 1
+  console.log('DB connected');
+ // console.log(mongoose.connection.readyState); //logs 1
+});
+mongoose.connection.on('disconnecting', () => {
+  console.log('DB disconnecting');
+  //console.log(mongoose.connection.readyState); // logs 3
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('DB disconnected');
+  //console.log(mongoose.connection.readyState); //logs 0
 });
 
-mongoose.connection.on('disconnected', () => {
-  console.log('disconnected');
-  console.log(mongoose.connection.readyState); //logs 0
-});
 
 app.use(morgan('dev'));
 app.use(bodyParser.json({limit: "5mb"}));
---
+++ backend/model/userModel.js
@@ -0,0 +1,53 @@
+const mongoose=require('mongoose');
+const bcrypt= require("bcryptjs");
+
+const userSchema= new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        trim: true,
+        required: [true, 'first name is required'],
+        maxlength: 32,
+    },
+
+    lastName: {
+        type: String,
+        trim: true,
+        required: [true, 'last name is required'],
+        maxlength: 32, 
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'email is required'],
+        unique: true,
+        match:[
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: [true, 'password is required'],
+        minlength: [true, 'password must have at least (6) characters'],
+    },
+
+    role: {
+        type: Number,
+        default: 0
+    }
+
+
+}, {timestamps: true});
+
+userSchema.pre('save' , async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    this.password=await bcrypt.hash(this.password,10);
+});
+
+module.exports = mongoose.model("User" , userSchema);
