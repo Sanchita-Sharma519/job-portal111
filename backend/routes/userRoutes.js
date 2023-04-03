5c35ebd38254cb6625c411af16c2180105ea8256
Sanchita-Sharma519 <sanchitasharma519@gmail.com>
2023-04-04 00:33:34 +0530

    created singleUser,editUser and deleteUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { allUsers } = require('../controllers/userController');
+const { allUsers, singleUser, editUser, deleteUser } = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
 
 
 router.get('/allusers',isAuthenticated,isAdmin,allUsers);
-
-
+router.get('/user/:id',isAuthenticated,singleUser);
+router.put('/user/edit/:id',isAuthenticated,editUser);
+router.delete('/admin/user/delete/:id',isAuthenticated,isAdmin,deleteUser);
 
 module.exports = router;
