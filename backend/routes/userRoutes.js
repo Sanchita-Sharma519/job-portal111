a43f71c4cb971cdf1b26ff0656088bb5aa4c17d2
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-04-04 00:52:10 +0530

    small changes made in user router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const { allUsers } = require('../controllers/userController');
-const { isAuthenticated } = require('../middleware/auth');
-
-
-router.get('/allusers',isAuthenticated,allUsers);
-
- 
-=======
 const { allUsers, singleUser, editUser, deleteUser } = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
 
@@ -17,6 +8,5 @@ router.get('/allusers',isAuthenticated,isAdmin,allUsers);
 router.get('/user/:id',isAuthenticated,singleUser);
 router.put('/user/edit/:id',isAuthenticated,editUser);
 router.delete('/admin/user/delete/:id',isAuthenticated,isAdmin,deleteUser);
->>>>>>> 5c35ebd38254cb6625c411af16c2180105ea8256
 
 module.exports = router;
